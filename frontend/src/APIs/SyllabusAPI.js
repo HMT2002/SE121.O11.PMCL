a1a7dd3cf542c9e286202367ca7d6b292382f17a
phatlam1811 <phatlam1811@gmail.com>
2023-11-18 06:06:15 +0700

    Add Frontend API controllers

---
+++ frontend/src/APIs/SyllabusAPI.js
@@ -0,0 +1,17 @@
+export const GET_Syllabuses = async () => {
+
+}
+
+export const GET_SyllabusById = async (id) => {
+
+}
+
+export const GET_SyllabusByAuthor = async (authorId) => {
+
+}
+
+export default SyllabusAPI = {
+    GET_Syllabus: GET_Syllabuses,
+    GET_SyllabusById,
+    GET_SyllabusByAuthor,
+}
---
+++ frontend/src/APIs/UserAPI.js
@@ -0,0 +1,37 @@
+export const GET_Users = () => {
+
+}
+
+export const GET_UserById = () => {
+
+}
+
+export const GET_UsersByDepartment = () => {
+
+}
+
+export const GET_UsersByCourse = () => {
+
+}
+
+export const POST_CreateUser = () => {
+
+}
+
+export const POST_UpdateUserById = () => {
+
+}
+
+export const DELETE_RemoveUserById = () => {
+
+}
+
+export default UserAPI = {
+    GET_Users,
+    GET_UserById,
+    GET_UsersByCourse,
+    GET_UsersByDepartment,
+    POST_CreateUser,
+    POST_UpdateUserById,
+    DELETE_RemoveUserById,
+}
