1a18650a0025c5480cb06cc5e679c190e658f467
DesDesDesHMT <82308907+HMT2002@users.noreply.github.com>
2024-02-02 14:04:27 +0700

    finish add outcome, assess to schedule

--- frontend/src/components/SyllabusInputPage/SyllabusInputPage.jsx
+++ frontend/src/components/SyllabusInputPage/SyllabusInputPage.jsx
@@ -1,8 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import './SyllabusInputPage.css';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import SyllabusAPI from '../../APIs/SyllabusAPI';
+import AuthContext from '../../contexts/auth-context';
+import { toast } from 'sonner';
 const OUT_COME_KEY = 'courseOutcomes';
 const ASSESSMENT_KEY = 'courseAssessments';
 const SCHEDULE_KEY = 'courseSchedules';
@@ -217,22 +220,19 @@ function ShowScheduleCourse({
               {item.courseOutcomes.map((courseOutcome, idxDetail) => (
                 <div className=" border p-2 mt-2 radius-1 border-yellow" key={idxDetail}>
                   <div className="w-full flex gap-3 align-center">
-                    <h3 className="text-yellow">Yêu cầu đầu ra {idxDetail + 1}</h3>
-
-                    {idxDetail === 0 ? null : (
-                      <div>
-                        <button
-                          type="button"
-                          className="text-red small"
-                          onClick={() => {
-                            console.log('click delete');
-                            onDeleteOutcome(index, courseOutcome);
-                          }}
-                        >
-                          <RemoveIcon />
-                        </button>
-                      </div>
-                    )}
+                    <h3 className="text-yellow">Yêu cầu đầu ra {courseOutcome.id}</h3>
+                    <div>
+                      <button
+                        type="button"
+                        className="text-red small"
+                        onClick={() => {
+                          console.log('click delete');
+                          onDeleteOutcome(index, courseOutcome);
+                        }}
+                      >
+                        <RemoveIcon />
+                      </button>
+                    </div>
                   </div>
 
                   <div className="flex gap-3 flex-wrap">
@@ -266,8 +266,8 @@ function ShowScheduleCourse({
                 <button
                   type="button"
                   className="small"
-                  onClick={() => {
-                    onAddCourseAssessElement?.(selectCourseAssess);
+                  onClick={(e) => {
+                    onAddCourseAssessElement(index, selectCourseAssess);
                     setSelectCourseAssess((prevState) => null);
                   }}
                 >
@@ -279,19 +279,16 @@ function ShowScheduleCourse({
               {item.courseAssessElements.map((courseAssessElement, idxDetail) => (
                 <div className=" border p-2 mt-2 radius-1 border-yellow" key={idxDetail}>
                   <div className="w-full flex gap-3 align-center">
-                    <h3 className="text-yellow">Thành phần đánh giá {idxDetail + 1}</h3>
-
-                    {idxDetail === 0 ? null : (
-                      <div>
-                        <button
-                          type="button"
-                          className="text-red small"
-                          onClick={() => onDeleteAsset(item.id, courseAssessElement.id)}
-                        >
-                          <RemoveIcon />
-                        </button>
-                      </div>
-                    )}
+                    <h3 className="text-yellow">Thành phần đánh giá {courseAssessElement.label}</h3>
+                    <div>
+                      <button
+                        type="button"
+                        className="text-red small"
+                        onClick={() => onDeleteAsset(index, courseAssessElement)}
+                      >
+                        <RemoveIcon />
+                      </button>
+                    </div>
                   </div>
 
                   <div className="flex gap-3 flex-wrap">
@@ -357,6 +354,7 @@ function ChoseOptions({ onSelected, selectedOption }) {
 function SyllabusInputPage(props) {
   const [start, setStart] = useState(true);
   const [selectedOption, setSelectedOption] = useState(null);
+  const authCtx = useContext(AuthContext);
   useEffect(() => {
     if (props.cloneSyllabusData !== undefined) {
       setSelectedOption((prevState) => {
@@ -381,7 +379,10 @@ function SyllabusInputPage(props) {
 
   const handleChoseOptions = (option) => {
     const data = [generateSchedules()];
+
     handleSetSelected(SCHEDULE_KEY, data);
+    handleSetSelected(ASSESSMENT_KEY, []);
+    handleSetSelected(OUT_COME_KEY, []);
   };
 
   const handleAddOutCome = (newOutcome, index) => {
@@ -419,35 +420,59 @@ function SyllabusInputPage(props) {
     }
     console.log(selectedOption);
     console.log(selectedOption.courseOutcomes);
-    let newOutcomeArr = [];
-
-    if (!selectedOption.courseSchedules[index].courseOutcomes) {
+    let newOutcomeArr = selectedOption.courseSchedules[index].courseOutcomes || [];
+    let newmainoutcomeArr = selectedOption.courseOutcomes || [];
+    if (selectedOption.courseSchedules[index].courseOutcomes) {
+      if (
+        selectedOption.courseSchedules[index].courseOutcomes.find((courseOutcome) => {
+          return courseOutcome.id === newOutcome.id;
+        }) === undefined
+      ) {
+        newOutcomeArr = [...newOutcomeArr, newOutcome];
+        if (
+          newmainoutcomeArr.find((courseoutcome) => {
+            return courseoutcome.id === newOutcome.id;
+          }) === undefined
+        ) {
+          newmainoutcomeArr = [...newmainoutcomeArr, newOutcome];
+        }
+      }
     } else {
-      newOutcomeArr = [...selectedOption.courseSchedules[index].courseOutcomes, newOutcome];
     }
     let newCourseSchedulesArr = [...selectedOption.courseSchedules];
     newCourseSchedulesArr[index].courseOutcomes = newOutcomeArr;
     console.log(newOutcome);
 
-    setSelectedOption((prev) => ({ ...prev, courseSchedules: newCourseSchedulesArr }));
+    setSelectedOption((prev) => ({
+      ...prev,
+      courseSchedules: newCourseSchedulesArr,
+      courseOutcomes: newmainoutcomeArr,
+    }));
     console.log(selectedOption);
   };
-  const handleDeleteOutcome = (index, course) => {
-    console.log({ index, course });
-    if (!course) {
+  const handleDeleteOutcome = (index, courseOutcome) => {
+    console.log({ index, courseOutcome });
+    if (!courseOutcome) {
       return;
     }
     console.log('Delete outcome');
     let newOutcomeArr = [];
-
+    let newoutcomeArr = selectedOption.courseOutcomes;
     if (!selectedOption.courseSchedules[index].courseOutcomes) {
     } else {
-      newOutcomeArr = [...selectedOption.courseSchedules[index].courseOutcomes.filter((t) => t.id !== course.id)];
+      newOutcomeArr = [
+        ...selectedOption.courseSchedules[index].courseOutcomes.filter((t) => t.id !== courseOutcome.id),
+      ];
+      // let checkarr = selectedOption.courseOutcomes.filter((outcome) => outcome.id === courseOutcome.id).length;
+      // console.log(checkarr);
+      // if (checkarr === 1) {
+      //   newoutcomeArr = newoutcomeArr.filter((outcome) => outcome.id !== courseOutcome.id);
+      // }
     }
     let newCourseSchedulesArr = [...selectedOption.courseSchedules];
     newCourseSchedulesArr[index].courseOutcomes = newOutcomeArr;
 
-    setSelectedOption((prev) => ({ ...prev, courseSchedules: newCourseSchedulesArr }));
+    setSelectedOption((prev) => ({ ...prev, courseSchedules: newCourseSchedulesArr, courseOutcomes: newoutcomeArr }));
     console.log(selectedOption);
   };
   const handleOnAddSchedule = () => {
@@ -473,34 +498,92 @@ function SyllabusInputPage(props) {
     setSelectedOption((prev) => ({ ...prev, [SCHEDULE_KEY]: cloneData }));
   };
 
-  const handleOnAddCourseAssessElement = (scheduleId) => {
-    if (!scheduleId || !selectedOption?.[SCHEDULE_KEY]?.length) return;
+  const handleOnAddCourseAssessElement = (index, newAssess) => {
+    // if (!scheduleId || !selectedOption?.[SCHEDULE_KEY]?.length) return;
 
-    const cloneData = [...selectedOption[SCHEDULE_KEY]];
+    // const cloneData = [...selectedOption[SCHEDULE_KEY]];
 
-    const index = cloneData.findIndex((t) => t.id === scheduleId);
+    // const index = cloneData.findIndex((t) => t.id === scheduleId);
 
-    if (index === -1) return;
+    // if (index === -1) return;
 
-    const newData = generateAssessElement();
+    // const newData = generateAssessElement();
 
-    cloneData[index].courseAssessElements.push(newData);
+    // cloneData[index].courseAssessElements.push(newData);
 
-    setSelectedOption((prev) => ({ ...prev, [SCHEDULE_KEY]: cloneData }));
+    // setSelectedOption((prev) => ({ ...prev, [SCHEDULE_KEY]: cloneData }));
+    console.log({ index, newAssess });
+
+    if (!newAssess) {
+      return;
+    }
+    console.log(selectedOption);
+    let newAssessArr = selectedOption.courseSchedules[index].courseAssessElements || [];
+    let newcourseassessElements = selectedOption.courseAssessments || [];
+    if (selectedOption.courseSchedules[index].courseAssessElements) {
+      if (
+        selectedOption.courseSchedules[index].courseAssessElements.find((courseAssess) => {
+          return courseAssess.label === newAssess.label;
+        }) === undefined
+      ) {
+        newAssessArr = [...newAssessArr, newAssess];
+        if (newcourseassessElements.find((courseAssess) => courseAssess.label === newAssess.label) === undefined) {
+          newcourseassessElements = [...selectedOption.courseAssessments, newAssess];
+        }
+      }
+    } else {
+    }
+    let newCourseSchedulesArr = [...selectedOption.courseSchedules];
+    newCourseSchedulesArr[index].courseAssessElements = newAssessArr;
+    console.log(newAssess);
+
+    setSelectedOption((prev) => ({
+      ...prev,
+      courseSchedules: newCourseSchedulesArr,
+      courseAssessments: newcourseassessElements,
+    }));
+    console.log(selectedOption);
   };
 
-  const handleOnDeleteAsset = (scheduleId, assetId) => {
-    if (!scheduleId || !selectedOption?.[SCHEDULE_KEY]?.length) return;
+  const handleOnDeleteAsset = (index, courseAssess) => {
+    // if (!scheduleId || !selectedOption?.[SCHEDULE_KEY]?.length) return;
 
-    const cloneData = [...selectedOption[SCHEDULE_KEY]];
+    // const cloneData = [...selectedOption[SCHEDULE_KEY]];
 
-    const index = cloneData.findIndex((t) => t.id === scheduleId);
+    // const index = cloneData.findIndex((t) => t.id === scheduleId);
 
-    if (index === -1) return;
+    // if (index === -1) return;
 
-    cloneData[index].courseAssessElements = [...cloneData[index].courseAssessElements.filter((t) => t.id !== assetId)];
+    // cloneData[index].courseAssessElements = [...cloneData[index].courseAssessElements.filter((t) => t.id !== assetId)];
 
-    setSelectedOption((prev) => ({ ...prev, [SCHEDULE_KEY]: cloneData }));
+    // setSelectedOption((prev) => ({ ...prev, [SCHEDULE_KEY]: cloneData }));
+
+    console.log({ index, courseAssess });
+    if (!courseAssess) {
+      return;
+    }
+    console.log('Delete assess');
+    let newAssessArr = selectedOption.courseSchedules[index].courseAssessElements || [];
+    let newcourseAssessArr = selectedOption.courseAssessments;
+    if (!selectedOption.courseSchedules[index].courseAssessElements) {
+    } else {
+      newAssessArr = [...newAssessArr.filter((t) => t.label !== courseAssess.label)];
+
+      // const checkarr = newcourseAssessArr.filter((assess) => assess.label === courseAssess.label).length;
+      // console.log(checkarr);
+      // if (checkarr === 1) {
+      //   newcourseAssessArr = newcourseAssessArr.filter((t) => t.label !== courseAssess.label);
+      // }
+    }
+    let newCourseSchedulesArr = [...selectedOption.courseSchedules];
+    newCourseSchedulesArr[index].courseAssessElements = newAssessArr;
+
+    setSelectedOption((prev) => ({
+      ...prev,
+      courseSchedules: newCourseSchedulesArr,
+      courseAssessments: newcourseAssessArr,
+    }));
+    console.log(selectedOption);
   };
 
   const handleOnChangeValueSchedule = (event, scheduleId) => {
@@ -521,9 +604,29 @@ function SyllabusInputPage(props) {
     setSelectedOption((prev) => ({ ...prev, [SCHEDULE_KEY]: cloneData }));
   };
 
-  const handleSubmit = () => {
-    console.log(selectedOption);
-    // props.onSubmit(selectedOption);
+  const handleSubmit = async () => {
+    try {
+      console.log(selectedOption);
+      selectedOption.course = '6562143b0192dcfeb0902e4b';
+      return;
+      const response = await SyllabusAPI.POST_CreateNewSyllabus(authCtx.token, selectedOption);
+
+      console.log(response);
+      if (response.status === 200) {
+        toast.success('Tạo mới đề cương thành công', {
+          duration: 2000,
+        });
+      } else {
+        toast.error('Lỗi không thể tạo mới đề cương', {
+          duration: 2000,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Lỗi không thể tạo mới đề cương', {
+        duration: 2000,
+      });
+    }
   };
 
   return (
